041dfe8a915259e7c4f04bae32acf151c0c77d81
Kasia Wrzesniak <kasia.wrzesniak@trivago.com>
2020-07-02 00:16:33 +0200

    Return RxJS Observable from the service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,7 +13,7 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
   constructor(private heroService: HeroService) {}
 
