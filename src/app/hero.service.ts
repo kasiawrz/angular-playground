167c5035cf1f303edb4d1d0cb13246ee89cc5075
Kasia Wrzesniak <kasia.wrzesniak@trivago.com>
2020-07-02 00:22:54 +0200

    Create MessageService (service-in-service)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +11,5 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return of(HEROES);
   }
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 }
